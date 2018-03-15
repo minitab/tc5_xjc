58af3261a24c9ef652ed6ee3f283feec2883a69b
minitab <xuquanfu_2004@163.com>
2018-03-15 23:47:13 +0800

    数据库设计更新

--- tc5_xjc.sql
+++ tc5_xjc.sql
@@ -9,7 +9,7 @@ CREATE TABLE `cmf_user` (
   `last_login_time` int(11) NOT NULL DEFAULT '0' COMMENT '最后登录时间',
   `score` int(11) NOT NULL DEFAULT '0' COMMENT '用户积分',
   `coin` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '金币',
-  `balance` decimal(10,2) NOT NULL DEFAULT '0.00' COMMENT '余额',
+  `balance` decimal(10,2) NOT NULL DEFAULT '0' COMMENT '余额',
   `create_time` int(11) NOT NULL DEFAULT '0' COMMENT '注册时间',
   `user_status` tinyint(3) unsigned NOT NULL DEFAULT '1' COMMENT '用户状态;0:禁用,1:正常,2:未验证',
   `user_login` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '用户名',
@@ -35,7 +35,7 @@ DROP TABLE IF EXISTS `cmf_goods`;
 CREATE TABLE `cmf_goods` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '商品id',
   `sn` varchar(24) NOT NULL DEFAULT '' COMMENT '商品货号',
-  `name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '商品名称',
+  `name` varchar(32) NOT NULL DEFAULT '' COMMENT '商品名称',
   `cate_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '产品分类id',
   `images` varchar(255) NOT NULL DEFAULT '' COMMENT '商品详情图',
   `thumbnail` varchar(64) NOT NULL DEFAULT '' COMMENT '商品缩略图',
@@ -43,8 +43,8 @@ CREATE TABLE `cmf_goods` (
   `description` text COMMENT '商品详情',
   `description_filtered` text COMMENT '处理过的商品详情',
   `keywords` varchar(64) NOT NULL DEFAULT '' COMMENT 'seo keywords',
-  `price` decimal(10,2) NOT NULL DEFAULT '0.00' COMMENT '商品价格',
-  `preferential_price` decimal(10,2) NOT NULL DEFAULT '0.00' COMMENT '商品优惠价',
+  `price` decimal(10,2) NOT NULL DEFAULT '0' COMMENT '商品价格',
+  `preferential_price` decimal(10,2) NOT NULL DEFAULT '0' COMMENT '商品优惠价',
   `brand` varchar(32) NOT NULL DEFAULT '' COMMENT '品牌',
   `model` varchar(64) NOT NULL DEFAULT '' COMMENT '型号',
   `unit` varchar(8) NOT NULL DEFAULT '' COMMENT '单位',
@@ -68,7 +68,7 @@ CREATE TABLE `cmf_goods` (
   `status` tinyint(2) unsigned NOT NULL DEFAULT '0' COMMENT '状态：-1删除；0未审核；1已审核',
   `more` text COMMENT '扩展属性,如缩略图;格式为json',
   PRIMARY KEY (`id`),
-  KEY `cate_brand_status` (`cate_id`,`brand`,`status`,`id`),
+  KEY `cateid_brand_status` (`cate_id`,`brand`,`status`),
   KEY `sn` (`sn`),
   KEY `name` (`name`),
   KEY `user_id` (`user_id`),
@@ -101,7 +101,7 @@ CREATE TABLE `cmf_combo` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '套餐id',
   `name` varchar(32) NOT NULL DEFAULT '' COMMENT '套餐名称',
   `description` varchar(255) NOT NULL DEFAULT '' COMMENT '套餐描述',
-  `price` decimal(10,2) NOT NULL DEFAULT '0.00' COMMENT '套餐价格',
+  `price` decimal(10,2) NOT NULL DEFAULT '0' COMMENT '套餐价格',
   `unit` varchar(8) NOT NULL DEFAULT '套' COMMENT '单位',
   `type` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '类型：1系统创建；2用户创建',
   `create_user_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '套餐创建用户id',
@@ -113,7 +113,9 @@ CREATE TABLE `cmf_combo` (
   `delete_time` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '删除时间',
   `status` tinyint(2) unsigned NOT NULL DEFAULT '1' COMMENT '状态：-1删除；0未审核；1已审核',
   `more` text COMMENT '扩展属性',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `type_status` (`type`,`status`),
+  KEY `createuserid_status` (`create_user_id`,`status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='套餐表';
 
 -- ----------------------------
@@ -125,7 +127,7 @@ CREATE TABLE `cmf_combo_info` (
   `combo_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '套餐id',
   `goods_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '商品id',
   `goods_num` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '商品数量',
-  `goods_unit_price` decimal(10,2) NOT NULL DEFAULT '0.00' COMMENT '商品单价',
+  `goods_unit_price` decimal(10,2) NOT NULL DEFAULT '0' COMMENT '商品单价',
   `unit` varchar(8) NOT NULL DEFAULT '' COMMENT '单位',
   `add_user_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '添加用户id',
   `add_time` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '添加时间',
@@ -133,7 +135,8 @@ CREATE TABLE `cmf_combo_info` (
   `list_order` int(8) NOT NULL DEFAULT '10000' COMMENT '排序：值越小排越前面',
   `status` tinyint(2) unsigned NOT NULL DEFAULT '1' COMMENT '状态：-1删除；0未审核；1已审核',
   `more` text COMMENT '扩展属性',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `comboid_status` (`combo_id`,`status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='套餐商品信息表';
 
 -- ----------------------------
@@ -158,7 +161,8 @@ CREATE TABLE `cmf_order` (
   `check_uid` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '结单确认人id',
   `status` tinyint(2) unsigned NOT NULL DEFAULT '1' COMMENT '状态：-1删除；0退单；1正常；8支付成功；9结单',
   `more` text COMMENT '扩展属性',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `createuserid_status` (`create_user_id`,`status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='订单表';
 
 -- ----------------------------
@@ -170,7 +174,7 @@ CREATE TABLE `cmf_order_info` (
   `order_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '订单id',
   `goods_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '商品id或套餐id',
   `goods_num` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '商品或套餐数量',
-  `goods_unit_price` decimal(10,2) NOT NULL DEFAULT '0.00' COMMENT '商品或套餐单价',
+  `goods_unit_price` decimal(10,2) NOT NULL DEFAULT '0' COMMENT '商品或套餐单价',
   `goods_unit` varchar(8) NOT NULL DEFAULT '' COMMENT '商品或套餐单位',
   `type` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '类型：1普通商品；2套餐',
   `is_special` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否特价：0否；1特价',
@@ -178,7 +182,8 @@ CREATE TABLE `cmf_order_info` (
   `delete_time` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '删除时间',
   `status` tinyint(2) unsigned NOT NULL DEFAULT '1' COMMENT '状态：-1删除；1正常',
   `more` text COMMENT '扩展属性',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `orderid_status` (`order_id`,`status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='订单商品信息表';
 
 -- ----------------------------
@@ -196,7 +201,7 @@ CREATE TABLE `cmf_address` (
   `county_name` varchar(32) NOT NULL DEFAULT '' COMMENT '省名',
   `address` varchar(255) NOT NULL DEFAULT '' COMMENT '详细地址',
   `person` varchar(32) NOT NULL DEFAULT '' COMMENT '联系人',
-  `mobile` char(16) NOT NULL DEFAULT '' COMMENT '联系电话',
+  `phone` char(16) NOT NULL DEFAULT '' COMMENT '联系电话',
   `is_default` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否默认地址：0否；1是',
   `add_time` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '添加时间',
   `delete_time` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '删除时间',
@@ -205,76 +210,93 @@ CREATE TABLE `cmf_address` (
   PRIMARY KEY (`id`),
   KEY `userid_status` (`user_id`,`status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='收货地址表';
-8. 收货地址address
 
-字段 | 类型（长度） | NULL/默认 | 特殊 | 备注
-:---:|:---:|:---:|:---:|:---:
-id | int(11) unsigned| | PRIMARY KEY | 主键
-uid | int(11) | | | 用户id
-province | int(11) | | | 省份
-city | int(11) | | | 市
-county | int(11) | | | 区县
-address | varchar(255) | | | 详细地址
-person | varchar(32) | | | 联系人
-phone | varchar(16) | | | 手机号
-is_default | tinyint(1) | 0 | | 是否默认地址：0否；1是
-9. 发票表invoice
+-- ----------------------------
+-- Table structure for `cmf_invoice`
+-- ----------------------------
+DROP TABLE IF EXISTS `cmf_invoice`;
+CREATE TABLE `cmf_invoice` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `user_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '用户id',
+  `title` varchar(255) NOT NULL DEFAULT '' COMMENT '发票抬头',
+  `no` varchar(24) NOT NULL DEFAULT '' COMMENT '票号',
+  `content_type` tinyint(2) unsigned NOT NULL DEFAULT '1' COMMENT '开票内容:1详细清单；2办公用品；3其他',
+  `price` decimal(10,2) NOT NULL DEFAULT '0' COMMENT '发票金额',
+  `tax` decimal(10,2) NOT NULL DEFAULT '0' COMMENT '税额',
+  `drawer_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '开票人id',
+  `create_time` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '开票日期',
+  `remarks` varchar(255) NOT NULL DEFAULT '' COMMENT '开票备注',
+  `status` tinyint(2) unsigned NOT NULL DEFAULT '1' COMMENT '状态：-1删除；0无效；1有效',
+  `more` text COMMENT '扩展属性',
+  PRIMARY KEY (`id`),
+  KEY `userid_status` (`user_id`,`status`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='发票表';
 
-字段 | 类型（长度） | NULL/默认 | 特殊 | 备注
-:---:|:---:|:---:|:---:|:---:
-id | int(11) unsigned| | PRIMARY KEY | 主键
-uid | | | | 用户id
-invoice_title | varchar(255) | | | 发票抬头
-invoice_no | varchar(24) | | | 税号
-invoice_content_type | tinyint(1) | | | 开票内容:1详细清单；2办公用品；3其他
-price | float(11,2) | | | 发票金额
-tax | float(11,2) | | | 税额
-drawer | varchar(32) | | | 开票人
-create_time | varchar(16) | | | 开票日期
-invoice_remarks | varchar(255) | NULL | | 开票备注
-status | | | | 状态：-1删除；0无效；1有效
-10. 发票明细表invoice_info
+-- ----------------------------
+-- Table structure for `cmf_invoice_info`
+-- ----------------------------
+DROP TABLE IF EXISTS `cmf_invoice_info`;
+CREATE TABLE `cmf_invoice_info` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `invoice_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '发票id',
+  `goods_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '商品id',
+  `goods_name` varchar(32) NOT NULL DEFAULT '' COMMENT '商品名称',
+  `goods_price` decimal(10,2) NOT NULL DEFAULT '0' COMMENT '商品单价',
+  `goods_num` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '商品数量',
+  `tax_rate` decimal(10,2) NOT NULL DEFAULT '0' COMMENT '税率',
+  `status` tinyint(2) unsigned NOT NULL DEFAULT '1' COMMENT '状态：-1删除；0无效；1有效',
+  `more` text COMMENT '扩展属性',
+  PRIMARY KEY (`id`),
+  KEY `invoiceid_status` (`invoice_id`,`status`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='发票明细表';
 
-字段 | 类型（长度） | NULL/默认 | 特殊 | 备注
-:---:|:---:|:---:|:---:|:---:
-id | int(11) unsigned | | PRIMARY KEY | 主键
-invoice_id | int(11) | | | 发票id
-goods_id | int(11) | | | 商品id（该字段待确认是否可省略，当出现办公用品时没有对应的goods_id）
-goods_name | varchar(32) | | | 商品名称
-goods_price | float(11,2) | | | 商品单价
-goods_num | int(11) | | | 商品数量
-tax_rate | float(11,2) | | | 税率
-status | tinyint(1) | 1 | | 状态：-1删除；0无效；1有效
-11. 公司管理表office
+-- ----------------------------
+-- Table structure for `cmf_office`
+-- ----------------------------
+DROP TABLE IF EXISTS `cmf_office`;
+CREATE TABLE `cmf_office` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `name` varchar(255) NOT NULL DEFAULT '' COMMENT '公司名字',
+  `province_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '省id',
+  `province_name` varchar(32) NOT NULL DEFAULT '' COMMENT '省名',
+  `city_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '市id',
+  `city_name` varchar(32) NOT NULL DEFAULT '' COMMENT '省名',
+  `county_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '区县id',
+  `county_name` varchar(32) NOT NULL DEFAULT '' COMMENT '省名',
+  `address` varchar(255) NOT NULL DEFAULT '' COMMENT '详细地址',
+  `phone` char(16) NOT NULL DEFAULT '' COMMENT '联系电话',
+  `invoice_title` varchar(255) NOT NULL DEFAULT '' COMMENT '发票抬头',
+  `invoice_sn` varchar(24) NOT NULL DEFAULT '' COMMENT '税号',
+  `status` tinyint(2) unsigned NOT NULL DEFAULT '1' COMMENT '状态：-1删除；1正常',
+  `more` text COMMENT '扩展属性',
+  PRIMARY KEY (`id`),
+  KEY `name_status` (`name`,`status`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='公司管理表';
 
-字段 | 类型（长度） | NULL/默认 | 特殊 | 备注
-:---:|:---:|:---:|:---:|:---:
-id | int(11) unsigned | | PRIMARY KEY | 主键
-office_name | varchar(32) | | | 公司名字
-office_province | int(11) | | | 公司所在省份
-office_city | int(11) | | | 公司所在市
-office_county | int(11) | | | 公司所在区县
-office_address | varchar(255) | | | 公司所在详细地址
-office_phone | varchar(16) | | | 公司电话
-invoice_title | varchar(32) | | | 发票抬头
-invoice_no | varchar(24) | | | 税号
-status | tinyint(1) | 1 | | 状态：-1删除；0不可用；1可用
-12. 积分记录表points_log
+-- ----------------------------
+-- Table structure for `cmf_points_log`
+-- ----------------------------
+DROP TABLE IF EXISTS `cmf_points_log`;
+CREATE TABLE `cmf_points_log` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `user_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '用户id',
+  `type` tinyint(2) unsigned NOT NULL DEFAULT '1' COMMENT '类型：1增加；2减少',
+  `points` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '积分',
+  `remarks` varchar(255) NOT NULL DEFAULT '' COMMENT '备注',
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='积分记录表';
 
-字段 | 类型（长度） | NULL/默认 | 特殊 | 备注
-:---:|:---:|:---:|:---:|:---:
-id | int(11) unsigned| | PRIMARY KEY | 主键
-uid | int(11) | | | 用户id
-type | tinyint(1) | | | 类型：1增加；2减少
-points | int(11) | | | 积分
-remarks | varchar(255) | | | 备注
-
-13. 充值记录表recharge_log
-
-字段 | 类型（长度） | NULL/默认 | 特殊 | 备注
-:---:|:---:|:---:|:---:|:---:
-id | int(11) unsigned| | PRIMARY KEY | 主键
-uid | int(11) | | | 用户id
-type | tinyint(1) | | | 充值方式：0线下；1微信
-money | float(11,2) | | | 积分
-remarks | varchar(255) | | | 备注
+-- ----------------------------
+-- Table structure for `cmf_recharge_log`
+-- ----------------------------
+DROP TABLE IF EXISTS `cmf_recharge_log`;
+CREATE TABLE `cmf_recharge_log` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `user_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '用户id',
+  `type` tinyint(2) unsigned NOT NULL DEFAULT '1' COMMENT '充值方式：0线下；1微信',
+  `money` decimal(10,2) NOT NULL DEFAULT '0' COMMENT '数量',
+  `remarks` varchar(255) NOT NULL DEFAULT '' COMMENT '备注',
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='充值记录表';
