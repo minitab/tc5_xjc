cb22a4a818c5b7f4a61a71b9e6d456bd05f34fc1
minitab <xuquanfu_2004@163.com>
2018-04-11 22:40:46 +0800

    运营管理

---
+++ app/shop/controller/AdminOperateController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Goodboy
+ * Date: 2018-4-11
+ * Time: 22:14
+ */
+namespace app\shop\controller;
+
+use cmf\controller\AdminBaseController;
+use think\Db;
+
+class AdminOperateController extends AdminBaseController
+{
+    /**
+     * 运营列表
+     *
+     * @return mixed
+     */
+    public function index(){
+
+
+        echo '运营列表';
+    }
+
+}
